a4eeb87f10facc5a0d616fc8cfc569354a58614a
Nick Taxeidis <n.taxeidis@codehub.gr>
2024-04-17 22:32:55 +0300

    Renamed ControllerAdvice.java

--- src/main/java/com/ntaxeidis/blog/config/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/ntaxeidis/blog/config/RestResponseEntityExceptionHandler.java
@@ -5,11 +5,13 @@ import com.ntaxeidis.blog.exception.PostNotFoundException;
 import com.ntaxeidis.blog.model.dto.ErrorMessage;
 import java.util.Date;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
-public class ControllerAdvice {
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler {
 	@ExceptionHandler(value = {PostNotFoundException.class, CommentNotFoundException.class})
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public ErrorMessage handleTokenRefreshException(PostNotFoundException ex, WebRequest request) {

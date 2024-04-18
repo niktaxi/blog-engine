dc2f00df23dddd40225ecd7ff7f3744dd4665090
Nick Taxeidis <n.taxeidis@codehub.gr>
2024-04-18 18:33:07 +0300

    Added @Builder and new fields

--- src/main/java/com/ntaxeidis/blog/model/dto/CommentDto.java
+++ src/main/java/com/ntaxeidis/blog/model/dto/CommentDto.java
@@ -1,19 +1,29 @@
 package com.ntaxeidis.blog.model.dto;
 
+import com.ntaxeidis.blog.model.Comment;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class CommentDto {
 
 	private Long id;
 
-	private String comment;
+	private String content;
 
 	private String author;
 
+	private Long likes;
+
 	private LocalDateTime creationDate;
+
+	private	LocalDateTime modifiedDate;
+
+	private List<Comment> thread;
 }
--- src/main/java/com/ntaxeidis/blog/model/dto/PostDto.java
+++ src/main/java/com/ntaxeidis/blog/model/dto/PostDto.java
@@ -1,11 +1,14 @@
 package com.ntaxeidis.blog.model.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
+@Builder
 @AllArgsConstructor
 public class PostDto {
 
@@ -14,4 +17,10 @@ public class PostDto {
 	private String content;
 
 	private LocalDateTime creationDate;
+
+	private	LocalDateTime modifiedDate;
+
+	private List<CommentDto> comments;
+
+	private Long likes;
 }
